95445cf63bdd56449a09806e058c7958a1a454f1
shirokuro <46kuro212@gmail.com>
2021-03-05 19:37:37 +0900

    confshelf.c

--- confshelf.c
+++ confshelf.c
@@ -19,28 +19,21 @@ static const int CMD_UNMANAGE = 0b01 << 5;
 static const int CMD_LINK = 0b01 << 6;
 static const int CMD_STATUS = 0b01 << 7;
 
-static void error(const char *msg, const char *cause)
-{
-	if (cause) {
-		fprintf(stdout, "ERROR: %s - %s\n", msg, cause);
-	} else {
-		fprintf(stdout, "ERROR: %s\n", msg);
-	}
-	exit(1);
-}
-
+// NULL if fail
 char* get_app_dir(char *suffix) 
 {
 	char *user_home = getenv("HOME");
 	if (!user_home)
 	{
-		error("\"HOME\" variable not set!", NULL);
+		fprintf(stderr, "\"HOME\" variable not set!\n");
+		return NULL;
 	}
 	char *middle = "/.confshelf/";
 	char *app_home = (char*) calloc(strlen(user_home) + strlen(middle) + strlen(suffix) + 1, sizeof(char));
 	if (app_home == NULL)
 	{
-		error("calloc fail", NULL);
+		fprintf(stderr, "calloc fail\n");
+		return NULL;
 	}
 	strcat(app_home, user_home);
 	strcat(app_home, middle);
@@ -48,17 +41,21 @@ char* get_app_dir(char *suffix)
 	return app_home;
 }
 
+// NULL if fail
 char* get_repo_dir_path()
 {
 	return get_app_dir("repo/");
 }
 
+// NULL if fail
 char* get_path_relative_to_repo_dir(char* suffix) {
 	char *repo_dir = get_repo_dir_path();
 	char *appended = (char*) calloc(strlen(repo_dir) + strlen(suffix) + 1, sizeof(char));
 	if (appended == NULL)
 	{
-		error("calloc failed", NULL);
+		fprintf(stderr, "calloc failed\n");
+		free(repo_dir);
+		return NULL;
 	}
 	strcat(appended, repo_dir);
 	strcat(appended, suffix);
@@ -98,12 +95,15 @@ void setup(char *argv[])
 	{
 		if (gerror == GIT_EEXISTS) 
 		{
-			error("confshelf is already set up!", NULL);
+			fprintf(stderr, "confshelf is already set up!\n");
+			goto cleanup;
 		}
 		const git_error *e = git_error_last();
-		error("Repository init failed", e->message);
+		fprintf(stderr, "Repository init failed - %s\n", e->message);
+		goto cleanup;
 	}
 	printf("Repository initialized!\n");
+cleanup:
 	git_repository_free(repo);
 	free(repo_dir);
 	repo_dir = NULL;
@@ -143,21 +143,31 @@ bool is_file(char *path)
 // Success: 0 Fail: != 0
 int insert_known_links(char *link_path, char *dst_path)
 {
+	int result = 0;
 	assert(link_path != NULL && dst_path != NULL);
 	char *known_links_path = get_known_links_file_path();
+	if (known_links_path == NULL)
+	{
+		return 1;
+	}
 	errno = 0;
 	FILE *known_links_file = fopen(known_links_path, "a");
 	if (known_links_file == NULL)
 	{
 		fprintf(stderr, "ERROR: Failed to open \"%s\": %s\n",
 				known_links_path, strerror(errno));
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	fprintf(known_links_file, "\"%s\" = \"%s\"\n", link_path, dst_path);
-	fclose(known_links_file);
+cleanup:
+	if (known_links_file != NULL)
+	{
+		fclose(known_links_file);
+	}
 	free(known_links_path);
 	known_links_path = NULL;
-	return 0;
+	return result;
 }
 
 int manage(int argc, char *argv[])
@@ -167,48 +177,59 @@ int manage(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s manage <src path> <dst path>\n", argv[0]);
 		return 1;
 	}
+	int result = 0;
 	char *src = argv[2];
 	char *dst_file_name = argv[3];
 	char *dst_path = get_path_relative_to_repo_dir(argv[3]);
 	if (!path_exists(src))
 	{
 		fprintf(stderr, "ERROR: \"%s\" not exists\n", src);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	if (!is_file(src)) {
 		fprintf(stderr, "ERROR: \"%s\" is not a regular file\n", src);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	if (path_exists(dst_path))
 	{
 		fprintf(stderr, "ERROR: \"%s\" already exists\n", dst_path);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	errno = 0;
 	if (rename(src, dst_path) != 0)
 	{
 		fprintf(stderr, "ERROR: Failed to move from \"%s\" to \"%s\": %s\n",
 				src, dst_path, strerror(errno));
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	errno = 0;
 	if (symlink(dst_path, src) != 0)
 	{
 		fprintf(stderr, "ERROR: Failed to create symlink (link: \"%s\", dst: \"%s\"): %s\n",
 				src, dst_path, strerror(errno));
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
-	int ins_result = insert_known_links(src, dst_file_name);
+	if (insert_known_links(src, dst_file_name) != 0)
+	{
+		result = 1;
+		goto cleanup;
+	}
+cleanup:
 	free(dst_path);
 	dst_path = NULL;
-	if (ins_result == 0)
+	if (result == 0)
 	{
 		printf("Success!\n");
-		return 0;
-	} else {
+	} else
+	{
 		printf("Failed\n");
-		return 1;
 	}
+	return result;
 }
 
 void unmanage(char *argv[])
@@ -222,42 +243,49 @@ int cmd_link(int argc, char *argv[])
 		printf("Usage: %s link <src path> <symlink path>\n", argv[0]);
 		return 1;
 	}
+	int result = 0;
 	char *src_file_name = argv[2];
 	char *src_path = get_path_relative_to_repo_dir(argv[2]);
 	char *symlink_path = argv[3];
 	if (!path_exists(src_path))
 	{
 		printf("ERROR: \"%s\" not exists\n", src_path);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	if (!is_file(src_path))
 	{
 		printf("ERROR: \"%s\" is not a regular file\n", src_path);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	if (path_exists(symlink_path))
 	{
 		printf("ERROR: \"%s\" already exists\n", symlink_path);
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
 	errno = 0;
 	if (symlink(src_path, symlink_path) != 0)
 	{
 		printf("ERROR: Failed to create symlink (link: \"%s\", dst: \"%s\"): %s\n",
 				symlink_path, src_path, strerror(errno));
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
-	int ins_result = insert_known_links(symlink_path, src_file_name);
-	free(src_path);
-	src_path = NULL;
-	if (ins_result == 0)
+	if (insert_known_links(symlink_path, src_file_name) == 0)
 	{
 		printf("Success!\n");
-		return 0;
-	} else {
+	} else
+	{
 		printf("Failed\n");
-		return 1;
+		result = 1;
+		goto cleanup;
 	}
+cleanup:
+	free(src_path);
+	src_path = NULL;
+	return result;
 }
 
 void status(char *argv[])
@@ -283,30 +311,78 @@ int conv_cmd_str_id(char *command)
 	return -1;
 }
 
-void init_dirs()
+// 0: success, 1: fail
+int init_config_file()
 {
-	char *app_root = get_app_dir("");
-	errno = 0;
-	if (mkdir(app_root, S_IRWXU) == -1 && errno != EEXIST)
+	int result = 0;
+	FILE *config_file = NULL;
+	char *config_path = get_config_file_path();
+	if (config_path == NULL)
 	{
-		char *errstr = errno != 0
-			? strerror(errno)
-			: "Unknown error";
-		error("Failed to create application root directory", errstr);
+		return 1;
 	}
-	char *repo_dir = get_repo_dir_path();
-	errno = 0;
-	if (mkdir(repo_dir, S_IRWXU) == -1 && errno != EEXIST)
+	if (path_exists(config_path))
 	{
-		char *errstr = errno != 0
-			? strerror(errno)
-			: "Unknown error";
-		error("Failed to repository directory", errstr);
+		if (is_file(config_path))
+		{
+			goto cleanup;
+		} else
+		{
+			fprintf(stderr, "Config file must be a regular file");
+			result = 1;
+			goto cleanup;
+		}
 	}
-	free(app_root);
-	app_root = NULL;
-	free(repo_dir);
-	repo_dir = NULL;
+	errno = 0;
+	config_file = fopen(config_path, "w");
+	if (config_file == NULL)
+	{
+		fprintf(stderr, "Failed to open configuration file: %s", strerror(errno));
+		result = 1;
+		goto cleanup;
+	}
+	char *initial_conf = "# Configuration format version.\n"
+		"# Do not modify this variable!\n"
+		"config_version = \"1\"\n"
+		"An absolute path to a repository.\n"
+		"The path doesn't have to be a git repository.\n"
+		"repository_path = \"\"\n";
+	fprintf(config_file, "%s", initial_conf);
+cleanup:
+	errno = 0;
+	if (config_file != NULL && fclose(config_file) != 0)
+	{
+		result = 1;
+		fprintf(stderr, "ERROR: fclose failed: %s", strerror(errno));
+	}
+	if (config_path != NULL)
+	{
+		free(config_path);
+	}
+	return result;
+}
+
+// 0: success, 1: fail
+int init_app_dir()
+{
+	char *app_dir_path = get_app_dir("");
+	errno = 0;
+	if (mkdir(app_dir_path, S_IRWXU) == -1 && errno != EEXIST)
+	{
+		fprintf(stderr, "Failed to create application root directory: %s", strerror(errno));
+		free(app_dir_path);
+		return 1;
+	}
+	free(app_dir_path);
+	return 0;
+}
+
+// 0: success, 1: fail
+int init_files()
+{
+	int result_dir = init_app_dir();
+	int result_conf = init_config_file();
+	return result_dir == 0 && result_conf == 0 ? 0 : 1;
 }
 
 int main(int argc, char *argv[])
@@ -316,7 +392,11 @@ int main(int argc, char *argv[])
 		usage(argv);
 		return 0;
 	}
-	init_dirs();
+	int init_result = init_files();
+	if (init_result != 0)
+	{
+		return 1;
+	}
 	int cmd = conv_cmd_str_id(argv[1]);
 	switch (cmd)
 	{
